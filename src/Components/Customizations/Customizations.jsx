28cd8b017a1836c5f29a9f2cb7df841272d2ac60
David <davsschwartz@yahoo.com>
2024-02-21 10:53:58 +0200

    feat: add customizations toggle

--- src/Components/Customizations/Customizations.jsx
+++ src/Components/Customizations/Customizations.jsx
@@ -3,38 +3,60 @@ import Title from "../Title";
 import ColorPicker from "../ColorPicker";
 import ResetButton from "../ResetButton";
 import { CustomizationsContext } from "./CustomizationsProvider";
-import TextInput from "../TextInput";
 import "./Customizations.css";
 import CustomizationsTextInput from "../CustomizationsTextInput/CustomizationsTextInput";
+import SettingsSVG from "../../assets/SettingsSVG";
 
 function Customizations() {
-  const { resetCustomizations, setPlayer1Char, setPlayer2Char} =
+  const { resetCustomizations, setPlayer1Char, setPlayer2Char } =
     React.useContext(CustomizationsContext);
+  const [showCustomizations, setShowCustomizations] = React.useState(false);
+
+  const toggleCustomizationsPanel = () => {
+    setShowCustomizations(!showCustomizations);
+  };
 
   return (
-    <section className='customizations-container'>
-      <fieldset className='customizations flex-column'>
-        <Title>Spice it up a bit!</Title>
-        <span className='margin-top'>Color:</span>
-        <div className='customizations margin-left'>
-          <ColorPicker of='X'>Player 1</ColorPicker>
-          <ColorPicker of='O'>Player 2</ColorPicker>
-        </div>
-        <div className='customizations flex-column'>
-          <div className='flex flex-column'>
-            Alternate <span>character for:</span>
-          </div>
+    <div
+      className='customizations-full-container'
+      style={{ right: showCustomizations ? "163px" : "0" }}
+    >
+      <section className='customizations-container'>
+        <fieldset className='customizations flex-column'>
+          <Title>Spice it up a bit!</Title>
+          <span className='margin-top'>Color:</span>
           <div className='customizations margin-left'>
-            <CustomizationsTextInput setCharacter={setPlayer1Char} defaultVal={'X'}/>
-            <CustomizationsTextInput setCharacter={setPlayer2Char} defaultVal={'O'} />
+            <ColorPicker of='X'>Player 1</ColorPicker>
+            <ColorPicker of='O'>Player 2</ColorPicker>
           </div>
-        </div>
-        <ResetButton
-          reset={resetCustomizations}
-          className='customizations-reset'
-        />
-      </fieldset>
-    </section>
+          <div className='customizations flex-column'>
+            <div className='flex flex-column'>
+              Alternate <span>character for:</span>
+            </div>
+            <div className='customizations margin-left'>
+              <CustomizationsTextInput
+                setCharacter={setPlayer1Char}
+                defaultVal={"X"}
+              />
+              <CustomizationsTextInput
+                setCharacter={setPlayer2Char}
+                defaultVal={"O"}
+              />
+            </div>
+          </div>
+          <ResetButton
+            reset={resetCustomizations}
+            className='customizations-reset'
+          />
+        </fieldset>
+      </section>
+      <button
+        className='customizations-toggle'
+        onClick={toggleCustomizationsPanel}
+      >
+        <SettingsSVG />
+      </button>
+    </div>
   );
 }
 
